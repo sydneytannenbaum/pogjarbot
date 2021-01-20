decacc0767ac27957df9ffb1c0166e8df649801d
Sydney Tannenbaum <slt7735@rit.edu>
2021-01-20 13:40:46 -0500

    Add !pog command

--- pogjar.py
+++ pogjar.py
@@ -14,7 +14,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!pog'):
+        await message.channel.send('Another one added to the jar!')
 
 #client.run(DISCORD_TOKEN)
