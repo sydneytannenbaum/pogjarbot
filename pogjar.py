801f6346329ea49c35b5cf5d69b4448e8fd3f51c
Michael Vaughan <mavaughan@protonmail.com>
2021-01-20 13:00:02 -0500

    added boilerplate code

---
+++ pogjar.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from settings import *
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+#client.run(DISCORD_TOKEN)
---
+++ settings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+DISCORD_TOKEN = getenv("DISCORD_TOKEN")
